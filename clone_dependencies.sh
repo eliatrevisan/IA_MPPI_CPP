4b9757d7304e2676d43d40141c9c07433a265b40
Elia Trevisan <73159802+eliatrevisan@users.noreply.github.com>
2024-08-07 14:25:37 +0200

    Update clone_dependencies.sh

--- clone_dependencies.sh
+++ clone_dependencies.sh
@@ -16,8 +16,8 @@ git clone https://github.com/catkin/catkin_simple.git
 git clone https://github.com/MoriKen254/timed_roslaunch.git
 # git clone https://github.com/tud-amr/amr-lmpcc.git
 
-git clone -b feature/add_vel_primitives git@github.com:stlucas44/asv_mppi.git
-git clone -b fix/ubuntu20.04 git@github.com:stlucas44/roboat_social_simulator.git
-git clone git@github.com:stlucas44/spline.git
+git clone -b feature/add_vel_primitives https://github.com/stlucas44/asv_mppi.git
+git clone -b fix/ubuntu20.04 https://github.com/stlucas44/roboat_social_simulator.git
+git clone https://github.com/stlucas44/spline.git
 
 git clone https://github.com/ros-planning/navigation.git
